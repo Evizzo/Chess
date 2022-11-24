e0d4d80a3fbd86aba00ae2c69c9bb23c4700717f
Evizzo <stefanmatic941@gmail.com>
2022-11-24 21:21:41 +0100

    Initial commit

---
+++ src/main/java/org/example/GameFrame.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GamePanel panel;
+
+    GameFrame(){
+        panel = new GamePanel();
+    //    this.add(panel);
+        this.setTitle("Chess");
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
