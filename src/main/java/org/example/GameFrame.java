1cb5735692f08ff10083b8ffdaddc825548b04ee
Evizzo <stefanmatic941@gmail.com>
2022-11-25 22:26:29 +0100

    Draw table.

--- src/main/java/org/example/GameFrame.java
+++ src/main/java/org/example/GameFrame.java
@@ -7,13 +7,15 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     GamePanel panel;
+    static final Dimension SCREEN_SIZE = new Dimension(512,548);
     GameFrame(){
         panel = new GamePanel();
         this.add(panel);
         this.setTitle("Chess");
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        this.setSize(SCREEN_SIZE);
         this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 }
--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -1,9 +1,28 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
-    GamePanel(){
+    public GamePanel(){
+        repaint();
+    }
 
+    @Override
+    public void paint(Graphics g) {
+        boolean colorSwitcher = true;
+        for(int y = 0; y<8; y++){
+            for(int x = 0; x<8; x++){
+                if(colorSwitcher){
+                    g.setColor(new Color(243, 243, 186));
+                }else{
+                    g.setColor(new Color(85, 148, 133));
+                }
+                g.fillRect(x * 64, y * 64, 64, 64);
+                colorSwitcher = !colorSwitcher;
+            }
+            colorSwitcher = !colorSwitcher;
+        }
     }
 }
+
