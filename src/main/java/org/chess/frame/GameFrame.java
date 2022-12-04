9e493e6c859dc326310a518553a11df73da5347a
Evizzo <stefanmatic941@gmail.com>
2022-12-04 17:57:01 +0100

    Fixed offset pixels

--- src/main/java/org/chess/frame/GameFrame.java
+++ src/main/java/org/chess/frame/GameFrame.java
@@ -1,19 +1,13 @@
 package org.chess.frame;
 
-import org.chess.board.pieces.Piece;
 import org.chess.panel.GamePanel;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
     GamePanel panel;
     static final Dimension SCREEN_SIZE = new Dimension(512, 512);
-    public static Piece selectedPiece = null;
-
     public GameFrame() {
         panel = new GamePanel();
         this.add(panel);
@@ -24,50 +18,6 @@ public class GameFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.pack();
         this.setVisible(true);
-
-        this.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                selectedPiece = Piece.getPiece(e.getX(), e.getY() - 36);
-            }
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                int getY = e.getY() - 36;
-                selectedPiece.move(e.getX() / 64, getY / 64);
-                panel.repaint();
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        this.addMouseMotionListener(new MouseMotionListener() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                if (selectedPiece != null){
-                    selectedPiece.realMouseXPosition = e.getX() - 36;
-                    selectedPiece.realMouseYPosition = e.getY() - 69;
-                    panel.repaint();
-                }
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent e) {
-
-            }
-        });
     }
 
 }
--- src/main/java/org/chess/panel/GamePanel.java
+++ src/main/java/org/chess/panel/GamePanel.java
@@ -5,8 +5,12 @@ import org.chess.board.pieces.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class GamePanel extends JPanel {
+    public static Piece selectedPiece = null;
     public GamePanel(){
         this.setPreferredSize(new Dimension(512, 512));
         new Pawn();
@@ -16,6 +20,47 @@ public class GamePanel extends JPanel {
         new King();
         new Queen();
         repaint();
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                selectedPiece = Piece.getPiece(e.getX(), e.getY());
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                selectedPiece.move(e.getX() / 64, e.getY() / 64);
+                repaint();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        this.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (selectedPiece != null){
+                    selectedPiece.realMouseXPosition = e.getX() - 36;
+                    selectedPiece.realMouseYPosition = e.getY() - 36;
+                    repaint();
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
     }
     @Override
     public void paint(Graphics g) {
